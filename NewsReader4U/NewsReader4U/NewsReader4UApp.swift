5ce7ba52b9f5d00c09a3bd5f247dcfb76ec46db8
iOS Solves <132999821+iossolves@users.noreply.github.com>
2024-08-09 09:49:05 +0530

    Add files via upload
    Initial Project creation

---
+++ NewsReader4U/NewsReader4U/NewsReader4UApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReader4UApp.swift
+//  NewsReader4U
+//
+//  Created by Karunakar Bandikatla on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsReader4UApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
